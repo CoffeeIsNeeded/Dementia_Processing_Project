c7b9251a2d2327045a7483e165acaafe9e01e846
CoffeeIsNeeded <95103501+CoffeeIsNeeded@users.noreply.github.com>
2023-12-31 22:31:04 +0000

    File Classes now have functions ready to be programed.

---
+++ Graph.py
@@ -0,0 +1,9 @@
+class Graph:
+    def __init__(self):
+        pass
+
+    def time_age(self):
+        pass
+    
+    def draw(self):
+        pass
---
+++ SQL.py
@@ -0,0 +1,21 @@
+class SQL:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def create_tables(self):
+        pass
+
+    def add_user(self):
+        pass
+
+    def get_users(self):
+        pass
+
+    def get_times(self):
+        pass
+
+    def get_age(self):
+        pass
